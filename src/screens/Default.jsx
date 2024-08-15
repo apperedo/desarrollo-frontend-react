cb4f3f95c512c4246eafbf824e9472adc78cc888
apperedo <76085026+apperedo@users.noreply.github.com>
2024-08-14 21:58:39 -0400

    avance clase 8-14

---
+++ src/screens/Default.jsx
@@ -0,0 +1,34 @@
+import Logo from './_components/Logo';
+import Description from './_components/Description';
+import OpenLink from '../components/OpenLink';
+import LessonInfo from './_components/LessonInfo';
+
+
+const Default = () => {
+    return (
+        <>
+            <header className="App-header">
+                <Logo />
+                <Description />
+                <OpenLink 
+                    title = "Learn React" 
+                    url = "https://react.dev/"
+                />
+                <OpenLink 
+                    title = "Mi Proyecto React (GitHub)" 
+                    url = "https://github.com/apperedo/desarrollo-frontend-react"
+                />
+                <LessonInfo 
+                    number = "1: " 
+                    title = "Introducción a react y estructura de proyecto"
+                />
+                <LessonInfo 
+                    number = "2: " 
+                    title = "Context API para la gestión del estado global en aplicaciones react"
+                />
+            </header>
+        </>
+    );
+};
+
+export default Default;
---
+++ src/screens/_components/GitHub.jsx
@@ -0,0 +1,16 @@
+const GitHub = () => {
+    return (
+        <div>
+            <a
+                className="App-link"
+                href="https://github.com/apperedo/desarrollo-frontend-react"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Mi Proyecto React (GitHub)
+            </a>
+        </div>
+    );
+};
+
+export default GitHub;
